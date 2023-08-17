e0f3fc63f38b6b95db53e677fdda0e2182661f58
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-08-18 00:08:18 +0900

    Post form render

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,20 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 5..50 }
   validates :body, presence: true, length: { minimum: 10 }
   validates :slug, presence: true, uniqueness: true
+
+  attr_accessor :tag_names
+
+  before_save :assign_tags_from_names
+
+  def assign_tags_from_names
+    return if tag_names.blank?
+
+    self.tags = tag_names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def tag_name_list
+    self.tags.map(&:name).join(', ')
+  end
 end
