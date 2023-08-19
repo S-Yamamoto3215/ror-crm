54635cfce01360bfbfa8b4d27dc5d3d0bc67eae1
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-08-19 23:39:41 +0900

    :sparkles: Restrict post creation to authenticated users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   has_many :classifications, dependent: :destroy
   has_many :tags, through: :classifications
+  belongs_to :user
 
   validates :title, presence: true, length: { in: 5..50 }
   validates :body, presence: true, length: { minimum: 10 }
