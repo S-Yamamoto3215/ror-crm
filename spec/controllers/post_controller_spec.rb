6a1f82a93c00dda52768a574c469da5f56960f6c
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2023-08-27 23:30:48 +0900

    :recycle: Use aggregate_failures for post#index

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe PostsController, type: :controller do
   describe '#index' do
     it 'responds successfully' do
       get :index
-      expect(response).to be_successful
-    end
 
-    it 'returns a 200 response' do
-      get :index
-      expect(response).to have_http_status '200'
+      aggregate_failures do
+        expect(response).to be_successful
+        expect(response).to have_http_status '200'
+      end
     end
   end
 
